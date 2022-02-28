b37623080e7dde5664b9aaf185f6654295499da0
Mateusz Gienieczko <mat@gienieczko.com>
2022-02-28 21:55:04 +0100

    Final polish before publish.

--- src/Sorcery/Shared/CourseBook.cs
+++ src/Sorcery/Shared/CourseBook.cs
@@ -25,19 +25,6 @@ public sealed class CourseBook
             {
                 new ("Basics", "basics", new SectionSkeleton[]
                 {
-                    new (".NET Taxonomy", "dotnet-taxonomy"),
-                    new ("Configuring Your Environment", "configuring-your-environment"),
-                    new ("Hello, World!", "hello-world"),
-                    new ("Basic Types", "basic-types"),
-                    new ("Control Flow", "control-flow"),
-                    new ("Arrays", "arrays"),
-                    new ("Classes", "classes"),
-                    new ("Inheritance", "inheritance"),
-                    new ("Abstract Types", "abstract-types"),
-                    new ("Strings", "strings"),
-                    new ("Attributes", "attributes"),
-                    new ("Testing with xUnit", "testing-with-xunit"),
-                    new ("Using GitHub Classroom", "using-github-classroom"),
-                }, new AssignmentSkeleton ("Dungeon Walker", "dungeon-walker"))
+                }, null)
             }));
 }
