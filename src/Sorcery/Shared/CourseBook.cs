dafd7094761fa6b1e40daffd6a77e3c9750ae23a
Mateusz Gienieczko <mat@gienieczko.com>
2022-04-27 09:35:21 +0200

    05 without assignment.

--- src/Sorcery/Shared/CourseBook.cs
+++ src/Sorcery/Shared/CourseBook.cs
@@ -75,6 +75,7 @@ public sealed class CourseBook
                     new ("Delegates", "delegates"),
                     new ("Lambda Expressions", "lambda-expressions"),
                     new ("LINQ Queries", "linq-queries"),
+                    new ("Local Methods", "local-methods"),
                 }, new AssignmentSkeleton("Layered Layouts", "layered-layouts", "https://classroom.github.com/a/wJNxPTTZ")),
                 new ("Asynchrony", "asynchrony", new SectionSkeleton[]
                 {
@@ -85,7 +86,7 @@ public sealed class CourseBook
                     new ("Async and Await", "async-and-await"),
                     new ("Thread Pool", "thread-pool"),
                     new ("Cancellation", "cancellation"),
-                }, new AssignmentSkeleton("Asynchronous Adventurers", "asynchronous-adventurers", "https://gienieczko.com/todo")),
+                }, new AssignmentSkeleton("Persisted Pathways", "persisted-pathways", "https://classroom.github.com/a/OsaBB_jJ")),
                 new ("Concurrency", "concurrency", new SectionSkeleton[]
                 {
                     new ("PLINQ", "plinq"),
