7f82792adb381a3400242c2a1bda6634a588c83a
Mateusz Gienieczko <mgienieczko@microsoft.com>
2022-11-30 17:59:24 +0000

    WIP on A-Z.

---
+++ src/BlogPostCode/AlphabetDoesNotGoAToZ/AlphabetDoesNotGoAToZ.UnitTests/SolutionUnitTests.cs
@@ -0,0 +1,66 @@
+﻿// Licensed under MIT, copyright Mateusz Gienieczko, all rights reserved.
+
+namespace AlphabetDoesNotGoAToZ.UnitTests
+{
+    public class SolutionUnitTests
+    {
+        private static readonly IReadOnlyList<string> BasicAlphaStrings = new[]
+        {
+            "helloworld", "HELLOWORLD", "HelloWorld", ""
+        };
+
+        private static readonly IReadOnlyList<string> BasicNonAlphaStrings = new[]
+        {
+            "Hello, World!", "h3110w0r1d", "hello_world", "hello::world"
+        };
+
+        private static readonly IReadOnlyList<string> NonLatinAlphaStrings = new[]
+        {
+            "Witajświecie", "Привiтсвiте", "سلامدنیا", "你好世界", "naïveSolutions"
+        };
+
+        private static IReadOnlyList<ISolution> CreateSolutions() => new ISolution[]
+        {
+            new IncorrectSolution(),
+            new IncorrectRegexSolution(),
+            new CorrectSolution(),
+            new CorrectRegexSolution(),
+        };
+
+        private static TheoryData<ISolution, string> CreateTestData(IEnumerable<string> strings)
+        {
+            var data = new TheoryData<ISolution, string>();
+
+            foreach (var implementation in CreateSolutions())
+            {
+                foreach (var text in strings)
+                {
+                    data.Add(implementation, text);
+                }
+            }
+
+            return data;
+        }
+
+        public static TheoryData<ISolution, string> BasicAlphaTestData => CreateTestData(BasicAlphaStrings);
+
+        public static TheoryData<ISolution, string> BasicNonAlphaTestData => CreateTestData(BasicNonAlphaStrings);
+
+        public static TheoryData<ISolution, string> NonLatinAlphaTestData => CreateTestData(NonLatinAlphaStrings);
+
+        [Theory]
+        [MemberData(nameof(BasicAlphaTestData))]
+        [MemberData(nameof(NonLatinAlphaTestData))]
+        public void IsAllLetters_WithAlphaStrings_ShouldBeTrue(ISolution solution, string text)
+        {
+            Assert.True(solution.IsAllLetters(text));
+        }
+
+        [Theory]
+        [MemberData(nameof(BasicNonAlphaTestData))]
+        public void IsAllLetters_WithNonAlphaStrings_ShouldBeFalse(ISolution solution, string text)
+        {
+            Assert.False(solution.IsAllLetters(text));
+        }
+    }
+}
---
+++ src/BlogPostCode/AlphabetDoesNotGoAToZ/AlphabetDoesNotGoAToZ/Solution.cs
@@ -0,0 +1,38 @@
+﻿// Licensed under MIT, copyright Mateusz Gienieczko, all rights reserved.
+
+using System.Text.RegularExpressions;
+
+namespace AlphabetDoesNotGoAToZ;
+
+public interface ISolution
+{
+    bool IsAllLetters(string text);
+}
+
+public sealed class IncorrectSolution : ISolution
+{
+    public bool IsAllLetters(string text) =>
+        text.All(c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+public sealed class IncorrectRegexSolution : ISolution
+{
+    // If you're not regex-savvy, this matches a full string that consists
+    // of an arbitrary number of characters either in a-z or A-Z.
+    private static readonly Regex AllLettersRegex = new("^[a-zA-Z]*$");
+
+    public bool IsAllLetters(string text) => AllLettersRegex.IsMatch(text);
+}
+
+public sealed class CorrectSolution : ISolution
+{
+    public bool IsAllLetters(string text) => text.All(c => char.IsLetter(c));
+}
+
+public sealed class CorrectRegexSolution : ISolution
+{
+    private static readonly Regex AllLettersRegex = new(@"^\p{L}*$");
+
+    public bool IsAllLetters(string text) => AllLettersRegex.IsMatch(text);
+}
+
