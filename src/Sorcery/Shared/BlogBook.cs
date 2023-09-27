846a04d3daf4b613a1d8068f1075935c60858315
Mateusz Gienieczko <mgienieczko@microsoft.com>
2023-09-27 11:28:08 +0100

    Try publish dev version.

--- src/Sorcery/Shared/BlogBook.cs
+++ src/Sorcery/Shared/BlogBook.cs
@@ -15,7 +15,7 @@ public sealed class BlogBook
     {
         _posts = new()
         {
-            new ("SIMD &ndash; Cheat Codes for Free Performance", "simd-cheat-codes-for-free-performance", null)
+            new ("SIMD &ndash; Cheat Codes for Free Performance", "simd-cheat-codes-for-free-performance", new DateTime(2023, 09, 27, 11, 35, 00, DateTimeKind.Utc))
             {
                 Tags = new Tag[]
                 {
