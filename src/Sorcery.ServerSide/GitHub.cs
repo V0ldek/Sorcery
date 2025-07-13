8cfd520b42f81c49806f024a5c82de852d0119b5
Mateusz Gienieczko <mat@gienieczko.com>
2025-07-13 17:41:31 +0200

    fully ported

--- src/Sorcery.ServerSide/GitHub.cs
+++ src/Sorcery.ServerSide/GitHub.cs
@@ -4,7 +4,12 @@ namespace Sorcery.ServerSide;
 
 public static class GitHub
 {
+    public const string AnyBlox = "https://github.com/AnyBlox";
+    
+    public const string Rsonpath = "https://github.com/rsonquery/rsonpath";
+    
     public const string Sorcery = "https://github.com/V0ldek/Sorcery";
 
     public const string CSharpNotebooks = "https://github.com/V0ldek/Sorcery-CSharp-Notebooks";
+    
 }
---
+++ src/Sorcery.ServerSide/wwwroot/scripts/app/scroll-to-top.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", (event) => {
+    const toTopButton = document.getElementsByClassName("mud-scroll-to-top")[0];
+    const topOffset = 300;
+    let visible = false;
+    document.addEventListener("scroll", (event) => {
+        if (!visible && document.documentElement.scrollTop >= topOffset) {
+            visible = true;
+            toTopButton.classList.add("visible");
+            toTopButton.classList.remove("hidden");
+        }
+        else if (visible && document.documentElement.scrollTop === 0) {
+            visible = false;
+            toTopButton.classList.remove("visible");
+            toTopButton.classList.add("hidden");
+        }
+    });
+    toTopButton.addEventListener("click", (event) => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    })
+});
