94423f94548713cd6b7f7450cc0e142b663036e4
Mateusz Gienieczko <mat@gienieczko.com>
2025-07-13 18:19:03 +0200

    fix titles

---
+++ src/Sorcery.ServerSide/wwwroot/scripts/app/title.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("DOMContentLoaded", (event) => {
+    const sourceryTitleTag = document.getElementById('sourcery-title-tag');
+    const actualTitleTag = document.getElementById('head-title-tag');
+    actualTitleTag.innerHTML = sourceryTitleTag.dataset['title'];
+});
